f8f5aaa20eb89113ef715c95ae65f4070f7e787c
Umut Tekin <tekinumut9@gmail.com>
2026-02-25 12:09:36 +0300

    Sprint 14 Gün 2 Projesi Tamamlandı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,34 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Wall wall1 = new Wall("North");
+        Wall wall2 = new Wall("South");
+        Wall wall3 = new Wall("East");
+        Wall wall4 = new Wall("West");
+
+        Ceiling ceiling = new Ceiling(250, PaintColor.WHITE);
+        Bed bed = new Bed("Modern", 4, 60, 2, 1);
+        Lamp lamp = new Lamp(LampType.NEON, true, 8);
+        Wardrobe wardrobe = new Wardrobe(200, 220, 85.5);
+        Carpet carpet = new Carpet(300, 400, PaintColor.RED);
+
+        Bedroom bedroom = new Bedroom(
+                "Master Bedroom",
+                wall1, wall2, wall3, wall4,
+                ceiling, bed, lamp, wardrobe, carpet
+        );
+
+        bedroom.getLamp().turnOn();
+        bedroom.getBed().make();
+        bedroom.getCarpet().lying();
+        bedroom.getWardrobe().add();
+        bedroom.getCeiling().create();
+        bedroom.getWall1().create();
     }
 }
